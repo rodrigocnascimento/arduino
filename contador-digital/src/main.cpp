3e81d66bc6e85a6a8e52a777d95432affa390a3c
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2019-03-30 22:31:51 -0300

    contador digiral

---
+++ contador-digital/src/main.cpp
@@ -0,0 +1,147 @@
+#include <Arduino.h>
+
+// Projeto 14 - Contador digital
+ 
+int segE = 7;
+int segD = 8;
+int segC = 9;
+int segB = 13;
+int segA = 12;
+int segF = 11;
+int segG = 10;
+ 
+void acende0()
+{
+  digitalWrite(segE, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segA, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);  
+}
+void acende1()
+{
+  digitalWrite(segE, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segA, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);  
+}
+void acende2()
+{
+  digitalWrite(segE, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segB, LOW);
+  digitalWrite(segA, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, LOW);  
+}
+void acende3()
+{
+  digitalWrite(segE, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segA, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, LOW);
+}
+void acende4()
+{
+  digitalWrite(segE, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segA, HIGH);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+void acende5()
+{
+  digitalWrite(segE, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segA, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+void acende6()
+{
+  digitalWrite(segE, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segA, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+void acende7()
+{
+  digitalWrite(segE, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segA, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+}
+void acende8()
+{
+  digitalWrite(segE, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segA, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+void acende9()
+{
+  digitalWrite(segE, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segA, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+
+void setup()
+{
+  pinMode(segE, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segA, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+}
+ 
+void loop()
+{
+  acende0();
+  delay(100);
+  acende1();
+  delay(100);
+  acende2();
+  delay(100);
+  acende3();
+  delay(100);
+  acende4();
+  delay(100);
+  acende5();
+  delay(100);
+  acende6();
+  delay(100);
+  acende7();
+  delay(100);
+  acende8();
+  delay(100);
+  acende9();
+  delay(100);
+}
